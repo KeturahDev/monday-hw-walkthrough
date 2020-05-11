e5857410d7d30a91a5bb8df50d7e5f611d6461af
Keturah Howard <keturah.dev@gmail.com>
2020-05-11 11:28:58 -0700

    create custome middleware
    Co-authored-by: jeremy kale padot <jeremy@padot.us>
    Co-authored-by: Patrick Delaney <prestwick97@gmail.com>
    Co-authored-by: Rachel Schieferstein <violenza@gmail.com>

---
+++ src/middleware/middleware-logger.js
@@ -0,0 +1,6 @@
+middlewareLogger = store => next => action => {
+  console.log('Original state:', store.getState());
+  console.log('Current Action:', action);
+  next(action);
+  console.log('New Updated State:', store.getState())
+}
