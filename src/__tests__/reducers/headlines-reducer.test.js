a86e30a2fc8a4ee50660dd8f3ffb70af87088338
Keturah Howard <keturah.dev@gmail.com>
2020-05-11 10:38:58 -0700

    write test for action that will be called in loading process
    Co-authored-by: jeremy kale padot <jeremy@padot.us>
    Co-authored-by: Patrick Delaney <prestwick97@gmail.com>
    Co-authored-by: Rachel Schieferstein <violenza@gmail.com>

--- src/__tests__/reducers/headlines-reducer.test.js
+++ src/__tests__/reducers/headlines-reducer.test.js
@@ -1,4 +1,5 @@
 import headlinesReducer from '../../reducers/headlines-reducer';
+import * as c from '../../actions/ActionTypes'
 
 describe('headlinesReducer', () => {
 
@@ -8,6 +9,8 @@ describe('headlinesReducer', () => {
     error: null
   };
 
+  let action;
+
   test('should successfully return the default state if no action is passed into it', () => {
     expect(headlinesReducer(defaultState, {type: null })).toEqual(
       {
@@ -16,5 +19,17 @@ describe('headlinesReducer', () => {
         error: null
       }
     );
+  });
+
+  test('requesting headlines should successfully change isLoading from false to true', () => {
+    action = {
+      type: c.REQUEST_HEADLINES
+    };
+
+    expect(headlinesReducer(defaultState, action)).toEqual({
+        isLoading: true,
+        headlines: [], 
+        error: null
+    });
   });
 });
