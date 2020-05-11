f926bc90075f0343c4f20c140136fdbbbe4b05ba
Keturah Howard <keturah.dev@gmail.com>
2020-05-11 13:54:27 -0700

    add middleware for logging state
    Co-authored-by: jeremy kale padot <jeremy@padot.us>
    Co-authored-by: Patrick Delaney <prestwick97@gmail.com>
    Co-authored-by: Rachel Schieferstein <violenza@gmail.com>

--- src/middleware/middleware-logger.js
+++ src/middleware/middleware-logger.js
@@ -1,6 +1,8 @@
-middlewareLogger = store => next => action => {
+const middlewareLogger = store => next => action => {
   console.log('Original state:', store.getState());
   console.log('Current Action:', action);
   next(action);
   console.log('New Updated State:', store.getState())
 }
+
+export default middlewareLogger;
